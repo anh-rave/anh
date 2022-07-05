33c77623ac5c3307a9335d3c9e81b1e992495a9d
Mateusz Bujnowicz <mateusz.bujnowicz@htdevelopers.com>
2022-07-05 03:21:22 +0100

    Add action button logic to homepage

---
+++ src/components/action_button/action_button.jsx
@@ -0,0 +1,5 @@
+import './style.css'
+
+const ActionButton = ({ onClick }) => <div className="actionbutton button" onClick={onClick}></div>
+
+export default ActionButton
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,20 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import ActionButton from '../../components/action_button/action_button'
 import Loader from '../../components/loader/loader'
 import './style.css'
 
 const Homepage = () => {
     let navigate = useNavigate()
 
+    const [loading, setLoading] = useState(true)
+    const [initialized, setInitialized] = useState(false)
+
     const goToEaster = () => navigate('/easter')
+
+    useEffect(() => {
+        setTimeout(() => setInitialized(true), 500)
+    }, [])
+
+    useEffect(() => {
+        if (!initialized) return
+
+        setTimeout(() => setLoading(false), 5000)
+    }, [initialized])
+
+    const getAction = () => {
+        if (!initialized) return <></>
+        if (loading) return <Loader />
+        return <ActionButton text="GO" />
+    }
 
     return (
         <>
             <div className="flexbox main">
                 <div className="homepage header">ANH.</div>
                 <div className="homepage easter" onClick={goToEaster} />
-                <div className="homepage action">
-                    <Loader />
-                </div>
+                <div className="homepage action">{getAction()}</div>
             </div>
         </>
     )
