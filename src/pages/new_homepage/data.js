f8883c8b251e5bda9f99262fa24ba2297af0c1a5
Mateusz Bujnowicz <mateusz.bujnowicz@cubbit.io>
2023-08-02 22:26:29 +0200

    update to anh i chuj 2

--- src/pages/new_homepage/data.js
+++ src/pages/new_homepage/data.js
@@ -3,7 +3,7 @@ const images = {
 }
 
 const getTimeLeftText = () => {
-    const totalHoursLeft = Math.floor((Date.parse('18 Aug 2023 19:00:00') - Date.now()) / (1000 * 60 * 60))
+    const totalHoursLeft = Math.floor((Date.parse('25 Aug 2023 19:00:00') - Date.now()) / (1000 * 60 * 60))
     if (totalHoursLeft < 0) {
         return 'ALREADY PASSED :/'
     }
@@ -18,7 +18,7 @@ const getTimeLeftText = () => {
 const texts = {
     0: 'ANNIHILATION PARTY',
     1: 'i chuj',
-    2: '18 SIERPNIA',
+    2: '25 SIERPNIA',
     3: getTimeLeftText(),
 }
 
